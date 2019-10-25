8ca83c150a86c9dc5922ebabaf3cef7ff24ca519
shirayr <44175517+shirayr@users.noreply.github.com>
2019-10-25 15:00:33 +0300

    the changes for execute on college computer added

--- Analysis_Species.py
+++ Analysis_Species.py
@@ -1,4 +1,10 @@
 '''
+Analtsis_Species.py
+Script that Goes through the species.out file and
+analyzes how many time steps have been for each num_Moles&num_Specs (the atom types and their amount)
+Usage:
+python Analtsis_Species.py
+The #'s are for exeute on the colleage computere
 '''
 import re
 import os
@@ -10,8 +16,9 @@ num_Moles = 0
 num_Specs = 0
 num_Timestep = 0
 inputFile = os.path.join(os.getcwd(),"species.out")
-#inputFile = open('{}/nvt_BB_real/species.out'.format(run_dir), 'r')
 file = open(inputFile, 'r')
+#inputFile_t = str('{}/nvt_BB_real/species.out'.format(run_dir))
+#file = open(inputFile_t, 'r')
 line = file.readline()
 while line:
 	# creating the reviews_list - an array of arrays that contains the full details required for each f1_f2	   
@@ -40,7 +47,8 @@ print(f1_f2_list)
 file.close()
 
 f1_f2_info_file = open(os.getcwd() + "\\result_f1=" + str(f1)+ "_f2=" + str(f2) + ".txt", 'w')
-#f1_f2_info_file = open('{}/nvt_BB_real/all_results_for_f1_f2/result_f1=' + str(f1)+ '_f2=' + str(f2) +'.txt'.format(run_dir), 'w')
+#f1_f2_info_file_t = str('{}/nvt_BB_real/all_results_for_f1_f2/result_f1='.format(run_dir)) + str(f1)+ "_f2=" + str(f2) +".txt"
+#f1_f2_info_file = open(f1_f2_info_file_t, 'w')
 str_forces = "f1 = " + str(f1) + " f2 = " + str(f2) + "\n"
 f1_f2_info_file.write(str_forces + str(f1_f2_list))
 f1_f2_info_file.close()
