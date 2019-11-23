72a8339ac3c89d7cf9edb18bb1fa29c38d049acf
shirayr <44175517+shirayr@users.noreply.github.com>
2019-11-23 19:22:37 +0200

    headers only D and E

--- info_to_CSV.py
+++ info_to_CSV.py
@@ -5,28 +5,28 @@ import csv
 inputFile = os.path.join(os.getcwd(),"resultCSV.txt")
 file = open(inputFile, 'r')
 line = file.readline()
-csv_file = open(os.getcwd() + "\\4_forces_last_step_500,000.csv", 'w')
+csv_file = open(os.getcwd() + "\\4_forces_last_step_506,000.csv", 'w')
 headers = str(line)[1:-2]
 headers = headers.replace("'","")
 headers = headers.replace(" ","")
 headers = headers.split(',')
 for i in range(len(headers)):
 	if headers[i] == 'C11H18N2':
-		headers[i] = 'C11H18N2 - D'
+		headers[i] = 'D' #'C11H18N2 - D'
 	if headers[i] == 'C19H20O4':
-		headers[i] = 'C19H20O4 - E'
+		headers[i] = 'E' #'C19H20O4 - E'
 	if headers[i] == 'C30H38O4N2':
-		headers[i] = 'C30H38O4N2 - DE'
+		headers[i] = 'DE' #'C30H38O4N2 - DE'
 	if headers[i] == 'C41H56O4N4':
-		headers[i] = 'C41H56O4N4 - D2E'
+		headers[i] = 'D2E' #'C41H56O4N4 - D2E'
 	if headers[i] == 'C49H58O8N2':
-		headers[i] = 'C49H58O8N2 - DE2'
+		headers[i] = 'DE2' #'C49H58O8N2 - DE2'
 	if headers[i] == 'C68H78O12N2':
-		headers[i] = 'C68H78O12N2 - DE3'
+		headers[i] = 'DE3' #'C68H78O12N2 - DE3'
 	if headers[i] == 'C79H96O12N4':
-		headers[i] = 'C79H96O12N4 - D2E3'
+		headers[i] = 'D2E3' #'C79H96O12N4 - D2E3'
 	if headers[i] == 'Other':
-		headers[i] = 'Other - D0E0'
+		headers[i] = 'D0E0' #'Other - D0E0'
 headers = str(headers)[1:-2]
 headers = headers.replace("'","")
 
